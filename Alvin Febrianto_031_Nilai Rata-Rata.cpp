c55385ab6712e489f81ae21319057ca511dc6805
Alvin <90168547+alvinfebrianto@users.noreply.github.com>
2021-09-22 18:56:49 +0700

    Update Alvin Febrianto_031_Nilai Rata-Rata.cpp

--- Alvin Febrianto_031_Nilai Rata-Rata.cpp
+++ Alvin Febrianto_031_Nilai Rata-Rata.cpp
@@ -3,10 +3,10 @@
 #include <math.h>
 using namespace std;
 
-main() {
+int main() {
 	float nilai_praktikum, nilai_teori, nilai_tugas, nilai_final_project;
 	
-	cout << " Program Menghitung Nilai Rata-Rata\n " << endl;
+	cout << " Program Menghitung Nilai Rata-Rata\n\n";
 	
 	cout << " Nilai Praktikum    : ";
 	cin >> nilai_praktikum;
@@ -18,8 +18,7 @@ main() {
 	cin >> nilai_final_project;
 	cout << endl;
 	
-	cout << " Nilai Rata-Rata = "
-	<< (nilai_praktikum*0.4)+(nilai_teori*0.4)+(nilai_tugas*0.1)+(nilai_final_project*0.1)/4.0 << endl;
+	cout << " Nilai Rata-Rata = " << (nilai_praktikum*0.4)+(nilai_teori*0.4)+(nilai_tugas*0.1)+(nilai_final_project*0.1)/4.0 << endl;
 	
 	getch();
 }
