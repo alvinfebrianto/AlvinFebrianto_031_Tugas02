e455af2929ec19725152c5b788b996106135a5de
Alvin <90168547+alvinfebrianto@users.noreply.github.com>
2021-09-22 19:02:52 +0700

    Update Alvin Febrianto_031_Cicilan.cpp

--- Alvin Febrianto_031_Cicilan.cpp
+++ Alvin Febrianto_031_Cicilan.cpp
@@ -3,25 +3,25 @@
 #include <math.h>
 using namespace std;
 
-main() {
+int main() {
 	int harga_pokok, harga_motor, lama_kredit, bunga, cicilan, total_harga_motor, keuntungan_dealer;
 	
-	cout << " Program Menghitung Kredit Motor\n " << endl;
+	cout << " Program Menghitung Kredit Motor\n\n";
 	cout << " Harga Motor        : ";
 	cin >> harga_motor;
 	cout << " Lama Kredit (Bulan): ";
 	cin >> lama_kredit;
 	cout << endl;
 	
-	harga_pokok=harga_motor/lama_kredit;
+	harga_pokok = harga_motor/lama_kredit;
 	cout << " Harga Pokok       = " << harga_pokok << endl;
-	bunga=harga_pokok*0.1;
+	bunga = harga_pokok*0.1;
 	cout << " Bunga             = " << bunga << endl;
-	cicilan=harga_pokok+bunga;
+	cicilan = harga_pokok+bunga;
 	cout << " Cicilan           = " << cicilan << endl;
-	total_harga_motor=cicilan*lama_kredit;
+	total_harga_motor = cicilan*lama_kredit;
 	cout << " Total Harga Motor = " << total_harga_motor << endl;
-	keuntungan_dealer=total_harga_motor-harga_motor;
+	keuntungan_dealer = total_harga_motor-harga_motor;
 	cout << " Keuntungan Dealer = " << keuntungan_dealer << endl;
 	
 	getch();
