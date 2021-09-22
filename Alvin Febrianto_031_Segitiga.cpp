ad6bff3bddba4ea1c99cb1d6055918f9b0e9de9a
Alvin <90168547+alvinfebrianto@users.noreply.github.com>
2021-09-22 18:53:05 +0700

    Update Alvin Febrianto_031_Segitiga.cpp

--- Alvin Febrianto_031_Segitiga.cpp
+++ Alvin Febrianto_031_Segitiga.cpp
@@ -3,19 +3,19 @@
 #include <math.h>
 using namespace std;
 
-main() {
+int main() {
 	int alas, tinggi, sisi_miring, keliling;
 	
-	cout << " Program Menghitung Sisi Miring dan Keliling Segitiga Siku-Siku\n " << endl;
+	cout << " Program Menghitung Sisi Miring dan Keliling Segitiga Siku-Siku\n\n";
 	cout << " Tinggi: ";
 	cin >> tinggi;
 	cout << " Alas  : ";
 	cin >> alas;
 	cout << endl;
 	
-	sisi_miring=sqrt(alas*alas+tinggi*tinggi);
+	sisi_miring = sqrt(alas*alas+tinggi*tinggi);
 	cout << " Sisi Miring = " << sisi_miring << endl;
-	keliling=alas+tinggi+sisi_miring;
+	keliling = alas+tinggi+sisi_miring;
 	cout << " Keliling    = " << keliling << endl;
 	
 	getch();
